38ec65d146e350be7a19df7a97e9cf1a7053b8af
Vezunchic <piratAlexander@yandex.ru>
2022-02-24 15:43:19 +0300

    Добавлено задание к Lesson_7

---
+++ Lesson_7/Lesson_7_ Test_1/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Lesson_7_ Test_1")]
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyProduct("Lesson_7_ Test_1")]
+[assembly: AssemblyTitle("Lesson_7_ Test_1")]
+[assembly: AssemblyVersion("1.0.0.0")]
